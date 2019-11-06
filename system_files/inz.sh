4349449317e0ec8dee1ad41ec5fce1b8fa98b92f
rgryta <rgryta@github.com>
2019-11-06 19:54:15 +0100

    Fix world and bump update rate

--- system_files/inz.sh
+++ system_files/inz.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#for i in {1..10}
-#do
-#   start=$SECONDS
-#   rosrun velma_common reset_shm_comm
-#   timeout 340s roscore &
-#   sleep 10
-#   timeout 330 roslaunch stero_velma offline_octo.launch &
-#   sleep 30
-#   timeout 300 roslaunch stero_velma rviz_gazebo.launch &
-#   sleep 30
-#   timeout 20 rosrun velma_task_cs_ros_interface initialize_robot.py
-#   timeout 250 rosrun stero_velma moveObject.py >> "${i}.txt"
-#   end=$SECONDS
-#   tosleep=$(( 400 - end + start ))
-#   sleep $tosleep
-#   pkill -P $$
-#done
-#clear
+for i in {1..10}
+do
+   start=$SECONDS
+   rosrun velma_common reset_shm_comm
+   timeout 340s roscore &
+   sleep 10
+   timeout 330 roslaunch stero_velma offline_octo.launch &
+   sleep 30
+   timeout 300 roslaunch stero_velma rviz_gazebo.launch &
+   sleep 30
+   timeout 20 rosrun velma_task_cs_ros_interface initialize_robot.py
+   timeout 250 rosrun stero_velma moveObject.py >> "${i}.txt"
+   end=$SECONDS
+   tosleep=$(( 400 - end + start ))
+   sleep $tosleep
+   pkill -P $$
+done
+clear
 echo "1st PART done"
 for i in {1..10}
 do
@@ -36,4 +36,4 @@ do
    pkill -P $$
 done
 clear
-echo "2nd part done
+echo "2nd part done"
