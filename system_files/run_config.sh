2c3d87a064e753776f06f1a66829447e352f7dd8
rgryta <rgryta@github.com>
2019-11-12 17:46:43 +0100

    x

--- system_files/run_config.sh
+++ system_files/run_config.sh
@@ -10,4 +10,5 @@ cp velma_sim_gazebo/lwr_gazebo.cpp2 $HOME/ws/ws_velma_os/src/velma_sim_gazebo/sr
 cd $HOME/ws/ws_velma_os
 
 catkin clean -y
+sudo rm /opt/ros/melodic/share/cmake_modules/cmake/Modules/FindUUID.cmake
 catkin build
